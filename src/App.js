4183e261ad45222ef972c569fe60ca04f623ae6e
Reanu <reanu123@gmail.com>
2026-04-10 21:17:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Home from './pages/Home';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import QuestionComponent from "./QuestionComponent";
 
 function App() {
   return (
     <div>
       <Home />
+      <BrowserRouter>
+      <Routers>
+        <route path="/" element={<QuestionComponent />} />
+      </Routers>
+      </BrowserRouter>
 
       {/* TODO: Replace this placeholder with Routes */}
     </div>
